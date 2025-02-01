9473d7e948db0d61fb1c9d0e4f5a41880f6e9c95
pbohea <pbohea@gmail.com>
2025-02-01 23:51:42 +0000

    working

--- app.rb
+++ app.rb
@@ -1,9 +1,36 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api = ENV.fetch("key")
+
+  exchange_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("key")}"
+
+  response = HTTP.get(exchange_url).to_s
+  response = JSON.parse(response)
+  currencies = response.fetch("currencies")
+  @xxx = currencies.keys
+ erb(:home)
+end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+  
+  exchange_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("key")}"
+  response = HTTP.get(exchange_url).to_s
+  response = JSON.parse(response)
+  currencies = response.fetch("currencies")
+  @xxx = currencies.keys
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("key")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  
+  # Some more code to parse the URL and render a view template.
 end
